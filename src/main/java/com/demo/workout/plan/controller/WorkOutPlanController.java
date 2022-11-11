23cfa64a9b3a5e42cf514fd25734f512a9c0d9e4
Chanyoung Kim <whqnd1106@gmail.com>
2022-11-11 22:39:17 +0900

    getWorkOutPlan 메소드 post 요청으로 변경

--- src/main/java/com/demo/workout/plan/controller/WorkOutPlanController.java
+++ src/main/java/com/demo/workout/plan/controller/WorkOutPlanController.java
@@ -3,9 +3,7 @@ package com.demo.workout.plan.controller;
 import com.demo.workout.plan.dto.WorkOutPlanDto;
 import com.demo.workout.plan.service.WorkOutPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -18,8 +16,8 @@ public class WorkOutPlanController {
     @Autowired
     WorkOutPlanService workOutPlanService;
 
-    @GetMapping("/getWorkOutPlan")
-    public Map<String, Object> getWorkOutPlan(WorkOutPlanDto workoutPlanDto) {
+    @PostMapping("/getWorkOutPlan")
+    public Map<String, Object> getWorkOutPlan(@RequestBody WorkOutPlanDto workoutPlanDto) {
 
         Map<String, Object> resultMap = new HashMap<>();
         String resultCd = "";
